7be6375145c0190edb919175533955c6e1fa5073
z123zero <z123zero@163.com>
2018-05-11 18:26:41 +0800

    test(analysis): test, del balance_high, balance_low

--- quant/analysis.py
+++ quant/analysis.py
@@ -52,22 +52,22 @@ def create_trade_log(completed_list, mult):
     平仓日期、平仓价格、执行类型、收益、佣金及总收益等
     """
     trade_log_list = []
-    # logger.info('completed_list: {}'.format(completed_list))
-    logger.info('len(completed_list): {}'.format(len(completed_list)))
+    # logger.debug('completed_list: {}'.format(completed_list))
+    logger.debug('len(completed_list): {}'.format(len(completed_list)))
     for i in completed_list:
-        # logger.info('i[0]: {}'.format(i[0]))
-        # logger.info('i[1]: {}'.format(i[1]))
+        # logger.debug('i[0]: {}'.format(i[0]))
+        # logger.debug('i[1]: {}'.format(i[1]))
         f = i[1]
 
         d = {}
         d['entry_date'] = i[0].date
         d['entry_price'] = i[0].price
         d['order_type'] = i[0].order_type
-        logger.info('i[0].date in analysis: {}'.format(i[0].date))
-        logger.info('i[0].price in analysis: {}'.format(i[0].price))
-        logger.info('i[0].order_type in analysis: {}'.format(i[0].order_type))
-        logger.info('i[0].units in analysis: {}'.format(i[0].units))
-        logger.info('i[1].units in analysis: {}'.format(i[1].units))
+        logger.debug('i[0].date in analysis: {}'.format(i[0].date))
+        logger.debug('i[0].price in analysis: {}'.format(i[0].price))
+        logger.debug('i[0].order_type in analysis: {}'.format(i[0].order_type))
+        logger.debug('i[0].units in analysis: {}'.format(i[0].units))
+        logger.debug('i[1].units in analysis: {}'.format(i[1].units))
         d['units'] = round(min(i[0].units, i[1].units), 3)
         d['exit_date'] = i[1].date
         d['exit_price'] = i[1].price
@@ -613,8 +613,8 @@ def stats(ts, trade_log, dbal, start, end, capital):
         datetime.strptime(dd['start_date'], "%Y-%m-%d %H:%M:%S"),
         datetime.strptime(dd['end_date'], "%Y-%m-%d %H:%M:%S")) * -1
     stats['drawdown_annualized_return'] = dd['max'] / cagr
-    dd = max_intra_day_drawdown(dbal['balance_high'], dbal['balance_low'])
-    stats['max_intra_day_drawdown'] = dd['max']
+    # dd = max_intra_day_drawdown(dbal['balance_high'], dbal['balance_low'])
+    # stats['max_intra_day_drawdown'] = dd['max']
     dd = rolling_max_dd(dbal['balance'], TRADING_DAYS_PER_YEAR)
     stats['avg_yearly_closed_out_drawdown'] = np.average(dd)
     stats['max_yearly_closed_out_drawdown'] = min(dd)

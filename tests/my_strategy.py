440a8b28556b6637d56f5b100f31d8b98fdc6a91
z123zero <z123zero@163.com>
2018-05-16 19:27:52 +0800

    fix(my_strategy):change letter case

--- tests/my_strategy.py
+++ tests/my_strategy.py
@@ -32,33 +32,35 @@ class MyStrategy(Strategy):
         logger.debug('self.balance[-1]: {}'.format(self.balance[-1]))
 
     def next(self):
-        MA2 = self.indicator.SMA(period=2, index=-1)
+        ma2 = self.indicator.SMA(period=2, index=-1)
         # MA5 = self.indicator.SMA(period=5, index=-1)
-        MA3 = self.indicator.SMA(period=3, index=-1)
+        ma3 = self.indicator.SMA(period=3, index=-1)
         # MA10 = self.indicator.SMA(period=10, index=-1)
         # MA20 = self.indicator.SMA(period=20, index=-1)
-        logger.info('MA2: {}'.format(MA2))
-        logger.info('MA3: {}'.format(MA3))
+        logger.info('MA2: {}'.format(ma2))
+        logger.info('MA3: {}'.format(ma3))
         # logger.info('MA10: {}'.format(MA10))
         # logger.info('MA20: {}'.format(MA20))
-        if MA2 > MA3:
-            if self.first_open:
+        if ma2 > ma3:
+            if self.first_open:  # 如果第一次开仓，则只能买入一次
                 self.buy(2)
                 self.first_open = False
                 return
             self.buy_even_and_open(2)
-        elif MA2 < MA3:
-            if self.first_open:
+            # self.buy(2)
+        elif ma2 < ma3:
+            if self.first_open:  # 如果第一次开仓，则只能卖出一次
                 self.sell(2)
                 self.first_open = False
                 return
             self.sell_even_and_open(2)
+            # self.sell(2)
 
 
 trade = Quant()
 data = CSVDataReader(
-    # datapath='../data/IF_cleaned_data.csv',
-    datapath='/home/demlution/桌面/quant/data/IF_cleaned_data.csv',
+    datapath='../data/IF_cleaned_data.csv',
+    # datapath='/home/demlution/桌面/quant/data/IF_cleaned_data.csv',
     instrument='IF',
     startdate='2015-04-06',
     enddate='2015-04-10')
@@ -67,7 +69,7 @@ portfolio = Portfolio
 strategy = MyStrategy
 
 trade.set_backtest(data_list, [strategy], portfolio)
-trade.set_commission(commission=0.0003, margin=0.15, mult=10, units=2, instrument='IF')
+trade.set_commission(commission=0.0003, margin=0.15, mult=300, units=2, instrument='IF')
 trade.set_cash(500000)
 trade.set_notify()
 trade.run()

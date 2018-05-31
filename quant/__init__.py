00ea8867569f7600550b1b454256a7ff93e1aca6
z123zero <z123zero@163.com>
2018-05-31 19:07:38 +0800

    fix(init):useless import

--- quant/__init__.py
+++ quant/__init__.py
@@ -1,14 +1,14 @@
-from quant.analysis import *
-from quant.backtestfill import *
-from quant.barbase import *
-from quant.broker import *
-from quant.dataseries import *
-from quant.dict_to_table import *
-from quant.event import *
-from quant.feedbase import *
-from quant.indicator import *
-from quant.logging_backtest import *
-from quant.main import *
-from quant.order import *
-from quant.portfolio import *
-from quant.strategy import *
+# from quant.analysis import *
+# from quant.backtestfill import *
+# from quant.barbase import *
+# from quant.broker import *
+# from quant.dataseries import *
+# from quant.dict_to_table import *
+# from quant.event import *
+# from quant.feedbase import *
+# from quant.indicator import *
+# from quant.logging_backtest import *
+# from quant.main import *
+# from quant.order import *
+# from quant.portfolio import *
+# from quant.strategy import *

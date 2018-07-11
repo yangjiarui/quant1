dbabeb2a680ed43817a449035fe54b81c3a28f9f
z123zero <z123zero@163.com>
2018-07-11 17:56:37 +0800

    feat(portfolio): change OrderEvent to context.OrderEvent

--- quant/portfolio.py
+++ quant/portfolio.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 from quant.event import OrderEvent, events
+from quant.context import Context
 
 
 class PortfolioBase(object):
@@ -9,7 +10,8 @@ class PortfolioBase(object):
 
     def generate_order(self):
         order = self.signal_event.order
-        events.put(OrderEvent(order))
+        order_event = Context().OrderEvent
+        events.put(order_event(order))
 
     def run_portfolio(self, signal_event):
         self.signal_event = signal_event

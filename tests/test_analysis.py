e1c6654b6b3beff0295fa5b7d1eb40e6b53d1824
z123zero <z123zero@163.com>
2018-07-26 17:53:12 +0800

    feat(test_analysis): add unittest for two functions

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,13 +2,18 @@
 import unittest
 import pandas as pd
 from quant import analysis
+from quant.context import Context
+from quant.logging_backtest import logger
+
+ohlc_data = pd.read_csv('IF_data.csv', index_col=0)
+ohlc_data.index = pd.DatetimeIndex(ohlc_data.index)
+context = Context()
+# context.fill.realized_gain_and_loss
 
 
 class TestAnalysis(unittest.TestCase):
     def test_get_trade_bars(self):
         op = analysis._true_func
-        ohlc_data = pd.read_csv('IF_data.csv', index_col=0)
-        ohlc_data.index = pd.DatetimeIndex(ohlc_data.index)
         trade_log = pd.DataFrame(columns=['date', 'position', 're_profit'])
         trade_log['date'] = [
             '2013/01/18', '2013/02/20', '2013/02/21', '2013/03/05',
@@ -18,9 +23,39 @@ class TestAnalysis(unittest.TestCase):
         trade_log['re_profit'] = list(range(12))
         lenth_bar1 = analysis._get_trade_bars(ohlc_data, trade_log, op)
         self.assertEquals(lenth_bar1, [19, 9, 3, 2, 8, 5])
-        trade_log.drop([0, 1])
+        logger.debug('trade_log: ', trade_log)
+        trade_log = trade_log.drop([0, 1]).reset_index(drop=True)
+        logger.debug('----------------------------------')
+        logger.debug('trade_log: ', trade_log)
         lenth_bar2 = analysis._get_trade_bars(ohlc_data, trade_log, op)
         self.assertEquals(lenth_bar2, [9, 3, 2, 8, 5])
+
+    def test_duration_of_equity_not_reaching_high(self):
+        equity = pd.DataFrame()
+        equity['date'] = [
+            '2013-01-07', '2013-01-08', '2013-01-09', '2013-01-10',
+            '2013-01-11', '2013-01-14', '2013-01-15', '2013-01-16',
+            '2013-01-17', '2013-01-18', '2013-01-21', '2013-01-22',
+            '2013-01-23', '2013-01-24', '2013-01-25', '2013-01-28',
+            '2013-01-29', '2013-01-30', '2013-01-31', '2013-02-01',
+            '2013-02-04', '2013-02-05', '2013-02-06', '2013-02-07',
+            '2013-02-08', '2013-02-18', '2013-02-19', '2013-02-20',
+        ]
+        equity.set_index('date', inplace=True)
+        equity.index = pd.DatetimeIndex(equity.index)
+        equity['equity'] = [
+            500000.00, 500000.00, 500000.00, 500000.00,
+            500000.00, 500000.00, 500000.00, 500000.00,
+            500000.00, 499765.06, 499105.06, 496765.06,
+            498325.06, 492385.06, 487105.06, 500305.06,
+            518485.06, 545065.06, 571885.06, 612685.06,
+            654385.06, 707725.06, 765325.06, 822325.06,
+            874465.06, 917065.06, 946705.06, 529521.12,
+        ]
+        logger.debug('---equity---: {}'.format(equity))
+        date = analysis.duration_of_equity_not_reaching_high(equity)
+        logger.debug('---date---: {}'.format(date))
+        self.assertEquals(date, '2013/01/07 - 2013/01/28')
 
 
 if __name__ == '__main__':
